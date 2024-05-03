a62d401e00349734f0a3dd0a346f357383a2367b
nBryan <nbryan133@hotmail.com>
2024-05-03 10:54:27 -0400

    Got everything working and now it is able to properly read and process NDFAs

--- Project2/ndfa.cpp
+++ Project2/ndfa.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <tuple>
+#include <vector>
 #include "ndfa,h"
 
 using namespace std;
@@ -57,7 +58,7 @@ void NDFA::addStackStart(char sym)
 
 //----------------------------------------------------------------
 void NDFA::displayNDFA() {
-    cout << "----------D F A----------" << endl;
+    cout << "----------N D F A----------" << endl;
     cout << "<States>\n";
 
     // Print states
@@ -79,24 +80,24 @@ void NDFA::displayNDFA() {
         cout << stackSym[i] << endl;
     }
 
-    cout << "<Transitions>\n";
+    cout << "<Transitions>\n" << flush;
 
     // Print transitions
     for (auto it = transitions.begin(); it != transitions.end(); it++) {
         const auto& stateSym = it->first;
         const auto& newState = it->second;
 
-        cout << " (" << get<0>(stateSym) << "," << get<1>(stateSym) << "," << get<2>(stateSym) << ") -> ";
+        cout << "(" << get<0>(stateSym) << "," << get<1>(stateSym) << "," << get<2>(stateSym) << ") -> ";
 
         // Print each transition in newState vector
         for (const auto& pair : newState) {
-            cout << "(" << pair.first << "," << pair.second;
+            cout << "(" << pair.first << "," << pair.second << ")" << endl;;
         }
 
-        cout << ")" << endl;
     }
 
     cout << "<Initial State>\n" << iState << endl;
+
     cout << "<Stack Start>\n" << iStack << endl;
 
     // Print final states
@@ -105,7 +106,7 @@ void NDFA::displayNDFA() {
         cout << fStates[i] << endl;
     }
 
-    cout << "-------------------------\n";
+    cout << "---------------------------\n";
 }
 
 
@@ -113,56 +114,92 @@ void NDFA::displayNDFA() {
 //----------------------------------------------------------------
 void NDFA::setCurAsInit()
 {
-    curState = (iState + " " + iStack);
+    string s(1, iStack);
+    curState = "";
+    curState = (iState + s);
 }
 
 //----------------------------------------------------------------
-void NDFA::transition(char sym)
+void NDFA::transition(char sym, char stackT)
 {
 
     string state = "";
     string stack = "";
 
+    bool accepted = false;
     bool isStack = false;
-    
-    for(int i = 0; i < curState.length(); i++)
-    {
-        if(curState[i] == ' ')
-        {
-            isStack = true;
-        }
 
-        if(isStack == false)
-        {
-            state += curState[i];
-        }
-        else
-        {
-            stack += curState[i];
-        }
-    }
+    state = curState.substr(0, 2);
+    stack = curState.substr(2, curState.length() - 2);
 
-    tuple<string, char, char> nextTransition = make_tuple(state, sym, stack[0]);   //makes tuple to look for transition
+    tuple<string, char, char> nextTransition = make_tuple(state, sym, stackT);   //makes tuple to look for transition
 
     auto it = transitions.find(nextTransition);
 
-    curState = "";
-
     int i = 0;
+
+    string newState;
 
     if (it != transitions.end())
     {
+        
+
         for (const auto &pair : it->second)
         {
-            curState += pair.first;
-            curState += pair.second;
-            i++;
-        }
-    }
+            newState = "";
 
-    if(i == 0)
-    {
-        i = 0;
+            /*cout << "sym: *" << sym << "*" << endl;
+            cout << "stack: *" << stack << "*" << endl;
+            cout << "stackT: *" << stackT << "*" << endl;
+
+            cout << "pairf: *" << pair.first << "*" << endl;
+            cout << "pairs: *" << pair.second << "*" << endl;
+            if(stack.length() > 0)
+            {
+                cout << "pairs+stack: *" << pair.second + stack.substr(1) << "*" << endl;
+            }*/
+
+            if(newState.length() == 0)
+            {
+                newState += pair.first;
+            }
+
+            if (!(pair.second.find("*") != std::string::npos))
+            {
+                if(stack.length() > 0 && pair.second != "" && stack.substr(1) != "")
+                {
+                    newState += pair.second;
+                    newState += stack.substr(1);
+                }
+                else if(pair.second != "")
+                {
+                    newState += pair.second;
+                }
+                
+            }
+            else 
+            {
+                if(stack.length() > 0)
+                {
+                    newState += stack.substr(1);
+                }
+                    
+            }
+
+            i++;
+
+        }
+
+        curState = "";
+
+        for(int i = 0; i < newState.length(); i++)
+        {
+            if(newState[i] != '\0')
+            {
+                curState += newState[i];
+            }
+        }
+
     }
 }
 
@@ -175,17 +212,21 @@ string NDFA::getCurState()
 //----------------------------------------------------------------
 bool NDFA::validCheck(char sym)
 {
-    bool isValid = false;
 
     for(int i = 0; i < symbols.size(); i++)
     {
         if(sym == symbols[i])
         {
-            isValid = true;
+            return true;
+        }
+
+        if(sym == '*')
+        {
+            return true;
         }
     }
 
-    return isValid;
+    return false;
 }
 
 //----------------------------------------------------------------
@@ -193,12 +234,7 @@ bool NDFA::finalCheck()
 {
     bool isFinal = false;
 
-    string state = "";
-
-    for(int i = 0; i < 2; i++)
-    {
-        state += curState[i];
-    }
+    string state = curState.substr(0, 2);
 
     for(int i = 0; i < fStates.size(); i++)
     {
@@ -212,3 +248,33 @@ bool NDFA::finalCheck()
 }
 
 //----------------------------------------------------------------
+
+vector<string> NDFA::possibleNextStates(string state, char sym, char stackT)
+{
+    string nextStates;
+
+    vector<string> possibleNextStates;
+
+    tuple<string, char, char> nextTransition = make_tuple(state, sym, stackT);   //makes tuple to look for transition
+    tuple<string, char, char> nextTwA = make_tuple(state, '*', stackT);   //makes tuple to look for transition for empty string top
+
+    auto it = transitions.find(nextTransition);
+
+    if (it != transitions.end())
+    {
+        
+        possibleNextStates.push_back(string(1, sym));
+        
+    }
+
+    it = transitions.find(nextTwA);
+
+    if (it != transitions.end())
+    {
+        
+        possibleNextStates.push_back("*");
+        
+    }
+
+    return possibleNextStates;
+}
--- Project2/program2.cpp
+++ Project2/program2.cpp
@@ -1,15 +1,20 @@
 //Name: Nate Bryan
 //File Name: program2.cpp
-//Date: 5th March, 2024
-//Description:
+//Date: 27th April, 2024
+//Description: Program to read and process ndfas from a given text file
 #include <iostream>
 #include <fstream>
 #include <csignal>
+#include <vector>
 #include "ndfa,h"
 
 using namespace std;
 
+vector<string> transitions;
+
 void signalHandler(int signal);
+
+bool process(NDFA newNDFA, string& uInput, int layer);
 
 //main function
 int main(int argc, char *argv[])
@@ -21,6 +26,10 @@ int main(int argc, char *argv[])
     string state2;
     string newT;
     string uInput;
+
+    string state = "";
+    string stack = "";
+    string curState;
 
     char sym;
     char stackT;
@@ -40,9 +49,9 @@ int main(int argc, char *argv[])
         fileName = argv[1];
     }
 
-    ifstream dfaFile(fileName);
+    ifstream ndfaFile(fileName);
 
-    if(!dfaFile.is_open())
+    if(!ndfaFile.is_open())
     {
         cout << "Error: unable to open file.\n";\
 
@@ -55,14 +64,16 @@ int main(int argc, char *argv[])
 
     NDFA newNDFA;
 
-    while(getline(dfaFile, nextLine))
+    while(getline(ndfaFile, nextLine))
     {
 
-        //nextLine.erase(std::remove_if(nextLine.begin(), nextLine.end(), [](unsigned char c) { return std::isspace(c); }), nextLine.end());
+        nextLine.erase(std::remove_if(nextLine.begin(), nextLine.end(), [](unsigned char c) { return std::isspace(c); }), nextLine.end());
+
+        //cout << "*" << nextLine << "*" << endl;
         
         if(nextLine[0] == '<')
         {
-            nextLine.erase(std::remove_if(nextLine.begin(), nextLine.end(), [](unsigned char c) { return std::isspace(c); }), nextLine.end());
+            //nextLine.erase(std::remove_if(nextLine.begin(), nextLine.end(), [](unsigned char c) { return std::isspace(c); }), nextLine.end());
         
             step = nextLine;
         }
@@ -70,45 +81,49 @@ int main(int argc, char *argv[])
         
 
         //add states to automata
-        if(step == "<states>" && nextLine[0] != '<')
+        if(step.find("<states>") != std::string::npos && nextLine[0] != '<')
         {
             newNDFA.addState(nextLine); 
         }
 
         //add alphabet to automata
-        else if(step == "<inputalphabet>" && nextLine[0] != '<')
+        else if(step.find("<inputalphabet>") != std::string::npos && nextLine[0] != '<')
         {
             newNDFA.addSymbol(nextLine[0]);
         }
 
         //add stack alphabet to automata
-        else if(step == "<stackalphabet>" && nextLine[0] != '<')
+        else if(step.find("<stackalphabet>") != std::string::npos && nextLine[0] != '<')
         {
             newNDFA.addStackSymbol(nextLine[0]);
         }
 
         //add transitions to automata
-        else if(step == "<transitions>" && nextLine[0] != '<')
+        else if(step.find("<transitions>") != std::string::npos && nextLine[0] != '<')
         {
-            for(int i = 0; i < 12; i++)
+            state1 = "";
+            state2 = "";
+            newT = "";
+
+            for(int i = 0; i < 8; i++)
             {
                 if(i < 2)
                 {
                     state1 += nextLine[i];
                 }
-                else if(i == 3)
+                else if(i == 2)
                 {
                     sym = nextLine[i];
                 }
-                else if(i == 5)
+                else if(i == 3)
                 {
                     stackT = nextLine[i];
                 }
-                else if( i > 6 && i < 9)
+                else if( i > 3 && i < 6)
                 {
                     state2 += nextLine[i];
                 }
-                else if( i > 9)
+                else if( i >=6  && nextLine[i] != ' ')
                 {
                     newT += nextLine[i];
                 }
@@ -121,136 +136,81 @@ int main(int argc, char *argv[])
             "\n newT: " << newT << endl << endl;*/
 
             newNDFA.addTransition(state1, sym, stackT, state2, newT);
-            state1 = "";
-            state2 = "";
-            newT = "";
         }
 
         //sets initial state for automata
-        else if(step == "<initialstate>" && nextLine[0] != '<')
+        else if(step.find("<initialstate>") != std::string::npos && nextLine[0] != '<')
         {
             newNDFA.addInitialState(nextLine);
         }
 
-        else if(step =="<stackstart>" && nextLine[0] != '<')
+        else if(step.find("<stackstart>")!= std::string::npos && nextLine[0] != '<')
         {
             newNDFA.addStackStart(nextLine[0]);
         }
 
         //sets final states to automata
-        else if(step == "<finalstates>" && nextLine[0] != '<')
+        else if(step.find("<finalstates>") != std::string::npos && nextLine[0] != '<')
         {
             newNDFA.addFinalStates(nextLine);
         }
     }
 
-    dfaFile.close();
+    newNDFA.addTransition("*", '*', '*', "*", "*");
+
+    ndfaFile.close();
 
     newNDFA.displayNDFA();
 
-    //loop for the user to input strings to be read by the automata
+    
     while(true)
     {
-
-        string state = "";
-        string stack = "";
-        string curState;
-
-        bool isStack = false;
-
-        newNDFA.setCurAsInit();
-        invalid = false;
-         
         cout << "Enter a string to process (CTRL^C to end): ";
         getline(cin, uInput);
         cout << endl;
 
-        isStack = false;
-        state = "";
-        stack = "";
+        newNDFA.setCurAsInit();
 
         curState = newNDFA.getCurState();
 
-        cout << "\n\ncurState: " << curState;
-
-        for(int i = 0; i < curState.length(); i++)
-        {
-            if(curState[i] == ' ')
-            {
-                isStack = true;
-            }
-
-            if(isStack == false)
-            {
-                state += curState[i];
-            }
-            else
-            {
-                stack += curState[i];
-            }
-        }
         
-        cout << " (" << state << "," << uInput << "," << stack << ")\n";
+        state = curState.substr(0, 2);
 
-        for(int i = 0; i < uInput.length(); i++)
-        {
-            if(!newNDFA.validCheck(uInput[i]))
-            {
-                cout << " (Invalid symbol " << uInput[i] << ")";
-                invalid = true;
-                break;
-            }
-
-            newNDFA.transition(uInput[i]);
-
-            isStack = false;
-            state = "";
-            stack = "";
-            curState = newNDFA.getCurState();
-
-            for(int i = 0; i < curState.length(); i++)
-            {
-                if(curState[i] == ' ')
-                {
-                    isStack = true;
-                }
-
-                if(isStack == false)
-                {
-                    state += curState[i];
-                }
-                else
-                {
-                    stack += curState[i];
-                }
-            }
-
-            cout << "|- " << "(" << state << ",";
-            
-            for(int n = i + 1; n <= uInput.length();n++)
-            {
-                if(i + 1 < uInput.length())
-                {
-                    cout << uInput[n];
-                }
-                else
-                {
-                    cout << "*";
-                }
-            }
-            
-            cout << "," << stack << ")\n";
-
-        }
-
-
-        if(newNDFA.finalCheck() && invalid == false)
-        {
-            cout << " : Accepted\n\n";
+        if(curState.length() > 2)
+        {  
+            stack = curState.substr(2, curState.length() - 2);
         }
         else
         {
-            cout << " : Rejected\n\n";
+            stack = "";
+        }
+        
+        
+
+        transitions.clear();
+
+        if(process(newNDFA, uInput, 0))
+        {
+
+            if(uInput.length() > 0)
+            {
+                cout << " (" << state << "," << uInput << "," << stack << ")\n";
+            }
+            else
+            {
+                cout << " (" << state << ",*," << stack << ")\n";
+            }
+
+            for(int i = transitions.size() - 1; i > -1 ; i--)
+            {
+                cout << "|-" << transitions.at(i);
+            }
+
+            cout << endl << "(Accepted)\n\n";
+        }
+        else
+        {
+            cout << "(Rejected)\n\n";
         }
     }
 
@@ -263,4 +223,114 @@ void signalHandler(int signal)
     cout << "\nExiting Program...\n";
 
     exit(signal);
+}
+
+bool process(NDFA newNDFA, string& uInput, int layer)
+{
+
+    if(!newNDFA.validCheck(uInput[0]) && uInput[0] != '*' && uInput.length() != 0)
+    {
+        cout << " (Invalid symbol " << uInput[0] << ")";
+        return false;
+    }
+
+    string state = "";
+    string stack = "";
+    string curState;
+
+    curState = newNDFA.getCurState();
+    state = curState.substr(0, 2);
+
+    if(curState.length() > 2)
+    {  
+        stack = curState.substr(2, curState.length() - 2);
+    }
+    else
+    {
+        stack = "*";
+    }
+
+    if(newNDFA.finalCheck() && uInput.length() == 0)
+    {
+        string transitionStr = "(" + state + ",*," + stack + ")\n";
+
+        if (std::find(transitions.begin(), transitions.end(), transitionStr) == transitions.end()) 
+        {
+            
+            transitions.push_back(transitionStr);
+            return true;
+        }
+    }
+
+    bool accepted = false;
+    bool isStack = false;
+
+    curState = newNDFA.getCurState();
+
+    vector<NDFA> ndfaBuffers;
+    vector<string> nextStates;
+
+    if(uInput[0] != '\0')
+    {
+        nextStates = newNDFA.possibleNextStates(state, uInput[0], stack[0]);
+    }
+    else
+    {
+        nextStates = newNDFA.possibleNextStates(state, '*', stack[0]);
+    }
+
+    for(int i = 0; i < nextStates.size(); i++)
+    {
+        NDFA s = *new NDFA(newNDFA);
+        ndfaBuffers.push_back(s);
+    }
+    
+    for(int i = 0; i < ndfaBuffers.size(); i++)
+    {
+        string newInput;
+
+        if(uInput.length() > 1 && nextStates.at(i).find('*') == std::string::npos)
+        {
+            newInput = nextStates.at(i) + uInput.substr(1);
+        }
+        else 
+        {
+            newInput = nextStates.at(i);
+        }
+
+        string input = newInput.substr(1);
+
+        ndfaBuffers.at(i).transition(newInput[0], stack[0]);
+
+        if(nextStates.at(i) != "*")
+        {
+            if(process(ndfaBuffers.at(i), input, (layer + 1)) == true)
+            {
+                accepted = true;
+            }
+        }
+        else
+        if(process(ndfaBuffers.at(i), uInput, (layer + 1)) == true)
+        {
+            accepted = true;
+        }
+    }
+
+    if(accepted == true)
+    {
+        if(uInput.length() > 0 && layer != 0)
+        {
+            transitions.push_back("(" + state + "," + uInput + "," + stack + ")\n");
+        }
+        else if(layer != 0)
+        {
+            transitions.push_back("(" + state + ",*," + stack + ")\n");
+
+        }
+
+        return true;
+    }
+
+    return false;
+
 }
